7af2d0a27c66d519e9a64997a1c06741fc24be9f
eminbilgen <46069858+eminbilgen@users.noreply.github.com>
2022-11-02 21:32:10 +0300

    i forgot adding linkedin

--- SOURCE CODE/src/components/Main.js
+++ SOURCE CODE/src/components/Main.js
@@ -8,6 +8,7 @@ import github from '../images/github.svg'
 import twitter from '../images/twitter.svg'
 import perfil from '../images/perfil.jpg'
 import insta from '../images/insta.svg'
+import linkedin from '../images/linkedin.svg'
 
 function Main() {
     return (
@@ -22,10 +23,13 @@ function Main() {
                 <a className="rounded-full hover:translate-y-2" href="https://github.com/eminbilgen/">
                     <img alt='githublogo' src={github} className="rounded-full w-10 h-10"></img>
                 </a>
-                <a className="rounded-full hover:-translate-y-2" href="https://twitter.com/eminbilgen00/">
+                <a className="rounded-full hover:-translate-y-2" href="https://www.linkedin.com/in/mustafa-emin-bilgen-aa4a24234/">
+                    <img alt='linkedinlogo' src={linkedin} className="rounded-full w-10 h-10"></img>
+                </a>
+                <a className="rounded-full hover:translate-y-2" href="https://twitter.com/eminbilgen00/">
                     <img alt='twlogo' src={twitter} className="rounded-full w-10 h-10"></img>
                 </a>
-                <a className="rounded-full hover:translate-y-2" href="https://instagram.com/eminbilgen/">
+                <a className="rounded-full hover:-translate-y-2" href="https://instagram.com/eminbilgen/">
                     <img alt='instalogo' src={insta} className="rounded-full w-10 h-10"></img>
                 </a>
             </div>
