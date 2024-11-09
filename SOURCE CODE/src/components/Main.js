64163d976abb66949ce61d37341d7e812187af1f
Emin Bilgen <46069858+eminbilgen@users.noreply.github.com>
2024-11-09 04:04:30 +0300

    update
    updated broken links and new photos

--- SOURCE CODE/src/components/Main.js
+++ SOURCE CODE/src/components/Main.js
@@ -16,14 +16,14 @@ function Main() {
         <div className='normal w-screen h-full flex flex-col justify-center items-center'>
             <div className='person w-96 h-96 flex flex-col justify-center items-center'>
                 <h1 className='flex text-4xl font-bold h-20 dark:text-white'>EMİN BİLGEN</h1>
-                <h1 className='flex ha text-xl font-semibold h-20 dark:text-white'>WEB DEVELOPER</h1>
+                <h1 className='flex ha text-xl font-semibold h-20 dark:text-white'>FULLSTACK WEB DEVELOPER</h1>
                 <img alt='mypicture' className='perfil rounded-full w-60 h-60 outline outline-offset-4 outline-8 hover:rotate-6 dark:outline-white' src={perfil}></img>
             </div>
             <div className='links w-60 h-10 flex justify-around pt-14'>
                 <a className="rounded-full hover:translate-y-2 h-10" href="https://github.com/eminbilgen/">
                     <Github alt='githublogo' className="rounded-full w-10 h-10"></Github>
                 </a>
-                <a className="rounded-full hover:-translate-y-2 h-10" href="https://www.linkedin.com/in/mustafa-emin-bilgen-aa4a24234/">
+                <a className="rounded-full hover:-translate-y-2 h-10" href="https://www.linkedin.com/in/emin-bilgen/">
                     <Linkedin alt='linkedinlogo' className="rounded-full w-10 h-10"></Linkedin>
                 </a>
                 <a className="rounded-full hover:translate-y-2 h-10" href="https://twitter.com/eminbilgen00/">
